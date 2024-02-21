16388780eaa0d04e9560b68c0b66d8a7e4857209
Cassiano Castro <development.castro@gmail.com>
2024-02-21 16:12:23 -0300

    Renamed directory: test => public

---
+++ public/FetchJS/index.js
@@ -0,0 +1,65 @@
+const chooser = document.querySelector("#chooser");
+const pre     = document.querySelector("body > pre");
+
+function updateDisplay(verse)
+{
+    verse = verse.replace(" ", "");
+    verse = verse.toLowerCase();
+
+    fetch(`${verse}.txt`)
+
+    .then((response) => {
+        if ( ! response.ok ) {
+            throw new Error(`HTTP Error: ${response.status}`);
+        }
+
+        return response.text();
+    })
+    .then((text) => {
+        pre.textContent = text;
+    })
+    .catch((error) => {
+        pre.textContent = "Couldn't fetch the resource...";
+    });
+}
+
+chooser.addEventListener("change", () => {
+    updateDisplay(chooser.value);
+});
+
+updateDisplay("Verse 1");
+
+chooser.value = "Verse 1";
+
+// class Person
+// {
+
+//     constructor(name, age)
+//     {
+//         this.name = name;
+//         this.age  = age;
+//     }
+
+//     getName()
+//     {
+//         return this.name;
+//     }
+
+//     getAge()
+//     {
+//         return this.age;
+//     }
+// }
+
+// class PersonFactory
+// {
+
+//     create(name = "", age = 18)
+//     {
+//         return new Person(name, age);
+//     }
+// }
+
+// let person = new PersonFactory().create("Cassiano", 24);
+
+// console.log(person instanceof Person);
