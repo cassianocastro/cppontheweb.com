f53ae00b06cf91aa717b89f0f5baecb379a1212c
Cassiano Castro <development.castro@gmail.com>
2024-03-01 17:43:21 -0300

    Added class

--- lib/FileReader.hpp
+++ lib/FileReader.hpp
@@ -2,6 +2,44 @@
 
 #define FILE_READER_HPP
 
+/**
+ *
+ */
+class FileReader
+{
 
+	private:
+		std::fstream stream;
+
+	public:
+		FileReader()
+		{
+			this->stream;
+		}
+
+		~FileReader()
+		{
+			this->stream.close();
+		}
+
+		std::string read(std::string filename)
+		{
+			std::string content(""), buffer("");
+
+			this->stream.open(filename, std::fstream::in);
+
+			if ( this->stream.is_open() )
+			{
+				while ( not this->stream.eof() )
+				{
+					getline(this->stream, buffer);
+
+					content.append(buffer);
+				}
+			}
+
+			return content;
+		}
+};
 
 #endif
